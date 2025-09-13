607ef347df7c7a3f29cb378f7f52474106f49b97
zohaibfast99 <zohaibrashid999@gmail.com>
2025-09-13 15:44:29 +0500

    Authentication working

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
-import Image from "next/image";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "@/components/ui/button"
+//import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
       <h1 className="text-6xl font-bold text-center">Welcome to Trackly</h1>
-      <p className="text-2xl text-center">
-        Get started by editing <code className="text-sm">pages/index.js</code>
-      </p>
-      <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Get Started
-      </button>
+      <div className="flex gap-4 mt-6">
+        <Button>
+          <RegisterLink>
+            Get Started
+          </RegisterLink>
+        </Button>
+        <Button asChild variant={"outline"}>
+          <LoginLink>
+            Sign In
+          </LoginLink>
+        </Button>
+      </div>
     </div>
   );
 }

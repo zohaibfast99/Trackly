927a462049319960c2edc8066d47924bcc3e6cd8
samar-2004 <samarjunaid1994@gmail.com>
2025-10-18 16:41:02 +0500

    fixed the page.tsx issue

--- app/(protected)/workspace/[workspaceId]/projects/[projectId]/page.tsx
+++ app/(protected)/workspace/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,23 +1,41 @@
-import { Tabs, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
-import React from "react"
-interface ProjectPageProps{
-    params: Promise<{workspaceId: string ; projectId:string}>;
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
+import React from "react";
 
-    searchParams: Promise<{[key:string]:string | string[]|undefined}>;
-    
+interface ProjectPageProps {
+  params: Promise<{ workspaceId: string; projectId: string }>;
+
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
-const ProjectPage =async (props:ProjectPageProps) =>{
-    const {workspaceId,projectId}=await props.params;
-     const searchParams=await props.searchParams;
+const ProjectPage = async (props: ProjectPageProps) => {
+  const { workspaceId, projectId } = await props.params;
+  const searchParams = await props.searchParams;
 
-    return <div className="flex flex-col gap-6 pb-3 px-3">
-        <Tabs defaultValue={(searchParams.view as string)|| "dashboard"} className="w-full">
-            <TabsList className="mb-4">
-                <link href="?view=dashboard">
-                <TabsTrigger value="dashboard" className="px-1.5 md:px-3">Dashboard</TabsTrigger></link>
-            </TabsList>
-        </Tabs>
-    </div>;
-    
+  return (
+    <div className="flex flex-col gap-6 pb-3 px-3">
+      <Tabs
+        defaultValue={(searchParams.view as string) || "dashboard"}
+        className="w-full"
+      >
+        <TabsList className="mb-4">
+          <Link href="?view=dashboard">
+            <TabsTrigger value="dashboard" className="px-1.5 md:px-3">
+              Dashboard
+            </TabsTrigger>
+          </Link>
+           <Link href="?view=table">
+            <TabsTrigger value="table" className="px-1.5 md:px-3">
+              Table
+            </TabsTrigger>
+          </Link>
+           <Link href="?view=kanban">
+            <TabsTrigger value="kanban" className="px-1.5 md:px-3">
+              Kanban
+            </TabsTrigger>
+          </Link>
+        </TabsList>
+      </Tabs>
+    </div>
+  );
 };
-export default ProjectPage
+export default ProjectPage;
